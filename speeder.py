5338738ef0eb32322d9e578832364ca5ba4429e8
SatyaDewangan05 <satyadewangan05@gmail.com>
2024-04-06 15:42:10 +0530

    First commit

---
+++ speeder.py
@@ -0,0 +1,19 @@
+# 1. Trim Silent Area
+def trim_silent_area():
+  pass
+
+# 2. Average Audio Speed for 5 min
+def calculate_avg_speed_of_audio(source_audios, target_audios):
+  pass
+
+# 3. Get the final time stemp of a complete audio segment (paragraph) by combining few sentences
+def get_paragraph_segment():
+  pass
+
+# 4. Calulate curve
+def calculate_curve(smooth_area, total_audio_len, avg_audio_speed):
+  pass
+
+# 5. Change the audio speed
+def audio_speed_up():
+  pass
