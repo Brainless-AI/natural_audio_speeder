111f8d458391672cb239cfdeca05c5790623df8d
Priyanshu-hawk <priyanshupatel.hawk@gmail.com>
2024-04-07 23:09:04 +0530

    - requirements update - triming silent audio parts from back - del unwanted audio files - code update speeder.py

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ from audiostretchy.stretch import stretch_audio
 
 
 # GET AUDIO SEGMENTS
-def get_audio_segments(audio_path, segment_dir, speed_up_start, speed_up_end, slow_down_start, slow_down_end):
+def get_audio_segments(audio_path, segment_dir, split_segment_length, speed_up_start, speed_up_end, slow_down_start, slow_down_end):
 
   # Load the audio
   sound = AudioSegment.from_wav(audio_path)
@@ -18,18 +18,18 @@ def get_audio_segments(audio_path, segment_dir, speed_up_start, speed_up_end, sl
   sound[:speed_up_start * 1000].export(f'{segment_dir}/0/segment-0.wav', format="wav")
 
   # 1: Speeding up section
-  for i in range(10):
-    start = (speed_up_start + (i/10)*(speed_up_end-speed_up_start)) * 1000 if not i == 0 else speed_up_start * 1000
-    end = (speed_up_start + ((i+1)/10)*(speed_up_end-speed_up_start)) * 1000 if not i == 9 else speed_up_end * 1000
+  for i in range(split_segment_length):
+    start = (speed_up_start + (i/split_segment_length)*(speed_up_end-speed_up_start)) * 1000 if not i == 0 else speed_up_start * 1000
+    end = (speed_up_start + ((i+1)/split_segment_length)*(speed_up_end-speed_up_start)) * 1000 if not i == (split_segment_length-1) else speed_up_end * 1000
     sound[start:end].export(f'{segment_dir}/1/segment-{i}.wav', format="wav")
 
   # 2: Constant speed section
   sound[speed_up_end * 1000:slow_down_start * 1000].export(f'{segment_dir}/2/segment-0.wav', format="wav")
   
   # 3: Speeding down section
-  for i in range(10):
-    start = (slow_down_start + (i/10)*(slow_down_end-slow_down_start)) * 1000 if not i == 0 else slow_down_start * 1000
-    end = (slow_down_start + ((i+1)/10)*(slow_down_end-slow_down_start)) * 1000 if not i == 9 else slow_down_end * 1000
+  for i in range(split_segment_length):
+    start = (slow_down_start + (i/split_segment_length)*(slow_down_end-slow_down_start)) * 1000 if not i == 0 else slow_down_start * 1000
+    end = (slow_down_start + ((i+1)/split_segment_length)*(slow_down_end-slow_down_start)) * 1000 if not i == (split_segment_length-1) else slow_down_end * 1000
     sound[start:end].export(f'{segment_dir}/3/segment-{i}.wav', format="wav")
 
   # 4: Last section (normal speed)
@@ -55,30 +55,58 @@ def merge_audio_files(input_dir, output_path):
     # Write the merged audio data to a new file
     sf.write(output_path, merged_data, samplerate)
 
+def remove_traling_zeros(input_file, output_file):
+    sound = AudioSegment.from_wav(input_file)
+
+    # convert audio to numpy array
+    np_array = sound.get_array_of_samples()
+
+    # remove trailing zeros
+    np_array = np.trim_zeros(np_array, 'b')
+
+    # build a new AudioSegment object from the numpy array
+    new_sound = AudioSegment(
+    data=np_array,
+    sample_width=sound.sample_width,
+    frame_rate=sound.frame_rate,
+    channels=sound.channels
+    )
+
+    return new_sound.export(output_file, format="wav")
+
 # GET PROGRESSIVE SPEEDING
 def get_progressive_speeding(input_dir, processing_dir, max_speed):
+    
     
     # 1: Speeding up section
     output_segment_dir = os.path.join(processing_dir, '1')
     os.makedirs(output_segment_dir, exist_ok=True)
-    for item in range(len(os.listdir(os.path.join(input_dir, '1')))):
+    total_items_len = len(os.listdir(os.path.join(input_dir, '1')))
+    for item in range(total_items_len):
         file = os.path.join(input_dir, '1', 'segment-'+str(item)+'.wav')
         output_segment_path = os.path.join(output_segment_dir, 'segment-'+str(item)+'.wav')
-        stretch_audio(file, output_segment_path, ratio=(1/(1+item*((max_speed-1)/10))))
+        stretch_audio(file, output_segment_path, ratio=(1/(1+item*((max_speed-1)/total_items_len))))
+        # will remove trailing zeros and save the file
+        remove_traling_zeros(output_segment_path, output_segment_path)
 
     # 2: Constant speed section
     output_segment_dir = os.path.join(processing_dir, '2')
     os.makedirs(output_segment_dir, exist_ok=True)
     file = os.path.join(input_dir, '2', 'segment-0.wav')
     stretch_audio(file, output_segment_path, ratio=(1/max_speed))
+    # will remove trailing zeros and save the file
+    remove_traling_zeros(output_segment_path, output_segment_path)
 
     # 3: Speeding down section
     output_segment_dir = os.path.join(processing_dir, '3')
     os.makedirs(output_segment_dir, exist_ok=True)
-    for item in range(len(os.listdir(os.path.join(input_dir, '3')))):
+    total_items_len = len(os.listdir(os.path.join(input_dir, '3')))
+    for item in range(total_items_len):
         file = os.path.join(input_dir, '3', 'segment-'+str(item)+'.wav')
         output_segment_path = os.path.join(output_segment_dir, 'segment-'+str(item)+'.wav')
-        stretch_audio(file, output_segment_path, ratio=(1/(max_speed-item*((max_speed-1)/10))))
+        stretch_audio(file, output_segment_path, ratio=(1/(max_speed-item*((max_speed-1)/total_items_len))))
+        # will remove trailing zeros and save the file
+        remove_traling_zeros(output_segment_path, output_segment_path)
 
 # MERGE FINAL AUDIO SEGMENTS
 def merge_final_audio_segments(input_dir, output_path):
